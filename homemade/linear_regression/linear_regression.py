3ef8568a2601dcaaa3b6bdd087c169d8d864d503
Bing Yang (bingy) <bingy@cisco.com>
2023-09-19 11:05:32 +0800

    cache code

--- homemade/linear_regression/linear_regression.py
+++ homemade/linear_regression/linear_regression.py
@@ -1,31 +1,24 @@
-"""Linear Regression Module"""
-
-# Import dependencies.
 import numpy as np
-from ..utils.features import prepare_for_training
+
+from homemade.linear_regression.utils.features import prepare_for_training
 
 
 class LinearRegression:
-    # pylint: disable=too-many-instance-attributes
-    """Linear Regression Class"""
-
     def __init__(self, data, labels, polynomial_degree=0, sinusoid_degree=0, normalize_data=True):
-        # pylint: disable=too-many-arguments
-        """Linear regression constructor.
+        """：
+        1.对数据进行预处理操作
+        2.先得到所有的特征个数
+        3.初始化参数矩阵
 
-        :param data: training set.
-        :param labels: training set outputs (correct values).
-        :param polynomial_degree: degree of additional polynomial features.
-        :param sinusoid_degree: multipliers for sinusoidal features.
-        :param normalize_data: flag that indicates that features should be normalized.
+        data:数据
+        polynomial_degree: 是否做额外变换
+        sinusoid_degree: 是否做额外变换
+        normalize_data: 是否标准化数据
         """
-
-        # Normalize features and add ones column.
-        (
-            data_processed,
-            features_mean,
-            features_deviation
-        ) = prepare_for_training(data, polynomial_degree, sinusoid_degree, normalize_data)
+        (data_processed,
+         features_mean,
+         features_deviation) = prepare_for_training(data, polynomial_degree, sinusoid_degree,
+                                                                         normalize_data)
 
         self.data = data_processed
         self.labels = labels
@@ -35,151 +28,88 @@ class LinearRegression:
         self.sinusoid_degree = sinusoid_degree
         self.normalize_data = normalize_data
 
-        # Initialize model parameters.
         num_features = self.data.shape[1]
         self.theta = np.zeros((num_features, 1))
 
-    def train(self, alpha, lambda_param=0, num_iterations=500):
-        """Trains linear regression.
-
-        :param alpha: learning rate (the size of the step for gradient descent)
-        :param lambda_param: regularization parameter
-        :param num_iterations: number of gradient descent iterations.
+    def train(self, alpha, num_iterations=500):
         """
+        训练模块，执行梯度下降得到theta值和损失值loss
 
-        # Run gradient descent.
-        cost_history = self.gradient_descent(alpha, lambda_param, num_iterations)
-
+        alpha: 学习率
+        num_iterations: 迭代次数
+        """
+        cost_history = self.gradient_descent(alpha, num_iterations)
         return self.theta, cost_history
 
-    def gradient_descent(self, alpha, lambda_param, num_iterations):
-        """Gradient descent.
-
-        It calculates what steps (deltas) should be taken for each theta parameter in
-        order to minimize the cost function.
-
-        :param alpha: learning rate (the size of the step for gradient descent)
-        :param lambda_param: regularization parameter
-        :param num_iterations: number of gradient descent iterations.
+    def gradient_descent(self, alpha, num_iterations):
         """
+        实际迭代模块
 
-        # Initialize J_history with zeros.
-        cost_history = []
+        alpha: 学习率
+        num_iterations: 迭代次数
 
-        for _ in range(num_iterations):
-            # Perform a single gradient step on the parameter vector theta.
-            self.gradient_step(alpha, lambda_param)
-
-            # Save the cost J in every iteration.
-            cost_history.append(self.cost_function(self.data, self.labels, lambda_param))
-
+        :return: 返回损失值 loss
+        """
+        cost_history = []  # 收集每次的损失值
+        for _ in range(num_iterations):  # 开始迭代
+            self.gradient_step(alpha)  # 每次更新theta
+            cost_history.append(self.cost_function(self.data, self.labels))
         return cost_history
 
-    def gradient_step(self, alpha, lambda_param):
-        """Gradient step.
-
-        Function performs one step of gradient descent for theta parameters.
-
-        :param alpha: learning rate (the size of the step for gradient descent)
-        :param lambda_param: regularization parameter
+    def gradient_step(self, alpha):
         """
+        梯度下降参数更新计算方法，注意是矩阵运算
 
-        # Calculate the number of training examples.
-        num_examples = self.data.shape[0]
-
-        # Predictions of hypothesis on all m examples.
-        predictions = LinearRegression.hypothesis(self.data, self.theta)
-
-        # The difference between predictions and actual values for all m examples.
-        delta = predictions - self.labels
-
-        # Calculate regularization parameter.
-        reg_param = 1 - alpha * lambda_param / num_examples
-
-        # Create theta shortcut.
+        alpha: 学习率
+        """
+        num_examples = self.data.shape[0]  # 当前样本个数
+        # 根据当前数据和θ获取预测值
+        prediction = LinearRegression.hypothesis(self.data, self.theta)
+        delta = prediction - self.labels  # 残差，即预测值减去真实值
         theta = self.theta
+        # 依照小批量梯度下降法，写代码表示
+        theta = theta - alpha * (1/num_examples)*(np.dot(delta.T, self.data)).T
+        self.theta = theta  # 计算完theta后更新当前theta
 
-        # Vectorized version of gradient descent.
-        theta = theta * reg_param - alpha * (1 / num_examples) * (delta.T @ self.data).T
-        # We should NOT regularize the parameter theta_zero.
-        theta[0] = theta[0] - alpha * (1 / num_examples) * (self.data[:, 0].T @ delta).T
-
-        self.theta = theta
-
-    def get_cost(self, data, labels, lambda_param):
-        """Get the cost value for specific data set.
-
-        :param data: the set of training or test data.
-        :param labels: training set outputs (correct values).
-        :param lambda_param: regularization parameter
+    def cost_function(self, data, labels):
         """
-
-        data_processed = prepare_for_training(
-            data,
-            self.polynomial_degree,
-            self.sinusoid_degree,
-            self.normalize_data,
-        )[0]
-
-        return self.cost_function(data_processed, labels, lambda_param)
-
-    def cost_function(self, data, labels, lambda_param):
-        """Cost function.
-
-        It shows how accurate our model is based on current model parameters.
-
-        :param data: the set of training or test data.
-        :param labels: training set outputs (correct values).
-        :param lambda_param: regularization parameter
+        损失计算方法，计算平均的损失而不是每个数据的损失值
         """
-
-        # Calculate the number of training examples and features.
         num_examples = data.shape[0]
-
-        # Get the difference between predictions and correct output values.
-        delta = LinearRegression.hypothesis(data, self.theta) - labels
-
-        # Calculate regularization parameter.
-        # Remember that we should not regularize the parameter theta_zero.
-        theta_cut = self.theta[1:, 0]
-        reg_param = lambda_param * (theta_cut.T @ theta_cut)
-
-        # Calculate current predictions cost.
-        cost = (1 / 2 * num_examples) * (delta.T @ delta + reg_param)
-
-        # Let's extract cost value from the one and only cost numpy matrix cell.
+        delta = LinearRegression.hypothesis(data, self.theta) - labels  # 预测值-真实值 得到残差
+        cost = np.dot(delta, delta.T)  # 损失值
         return cost[0][0]
-
-    def predict(self, data):
-        """Predict the output for data_set input based on trained theta values
-
-        :param data: training set of features.
-        """
-
-        # Normalize features and add ones column.
-        data_processed = prepare_for_training(
-            data,
-            self.polynomial_degree,
-            self.sinusoid_degree,
-            self.normalize_data,
-        )[0]
-
-        # Do predictions using model hypothesis.
-        predictions = LinearRegression.hypothesis(data_processed, self.theta)
-
-        return predictions
 
     @staticmethod
     def hypothesis(data, theta):
-        """Hypothesis function.
-
-        It predicts the output values y based on the input values X and model parameters.
-
-        :param data: data set for what the predictions will be calculated.
-        :param theta: model params.
-        :return: predictions made by model based on provided theta.
         """
+        获取预测值
 
-        predictions = data @ theta
+        :param data:  矩阵数据
+        :param theta:  权重θ
+        :return: 返回预测值
+        """
+        predictions = np.dot(data, theta)
+        return predictions
+
+    def get_cost(self, data, labels):
+        """
+        得到当前损失
+        """
+        data_processed = prepare_for_training(data,
+                                                                   self.polynomial_degree,
+                                                                   self.sinusoid_degree,
+                                                                   self.normalize_data)[0]
+        return self.cost_function(data_processed, labels)
+
+    def predict(self, data):
+        """
+        用训练的参数模型，预测得到回归值的结果
+        """
+        data_processed = prepare_for_training(data,
+                                                                   self.polynomial_degree,
+                                                                   self.sinusoid_degree,
+                                                                   self.normalize_data)[0]
+        predictions = LinearRegression.hypothesis(data_processed, self.theta)
 
         return predictions
